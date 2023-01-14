2a062b0f491b41677755091221b4309c358bd6d5
Fran Linde Blazquez <franlindebl@gmail.com>
2023-01-14 23:42:56 +0100

    Added React.Lazy and React.Suspense sample with InterSectionObserver

--- src/components/ApisInfoGroup.js
+++ src/components/ApisInfoGroup.js
@@ -1,11 +1,44 @@
 
-import PokemonInfo from "./PokemonInfo";
-import StarwarsInfo from "./StarWarsInfo";
+import React from "react";
+// import PokemonInfo from "./PokemonInfo";
+// import StarwarsInfo from "./StarWarsInfo";
+
+// Listado de componentes lazy
+const PokemonInfoLazy = React.lazy(() => import("./PokemonInfo"));
+const StarwarsInfoLazy = React.lazy(() => import("./StarWarsInfo"));
 
 const ApisInfoGroup = () => {
+  // Estados
+  const [showComponents, setShowComponents] = React.useState(false);
+
+  // Referencias
+  const myRef = React.useRef(null);
+
+  React.useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((element) => {
+        if (element.isIntersecting) {
+          setShowComponents(true);
+        }
+      });
+    });
+
+    observer.observe(myRef.current);
+  }, []);
+
   return <>
-    <PokemonInfo></PokemonInfo>
-    <StarwarsInfo></StarwarsInfo>
+
+    <div ref={myRef} style={{ 'borderTop': '1px solid red' }}></div>
+
+    {
+      showComponents ?
+        <React.Suspense fallback={<p>Cargando...</p>}>
+          <PokemonInfoLazy></PokemonInfoLazy>
+          <StarwarsInfoLazy></StarwarsInfoLazy>
+        </React.Suspense> :
+        <p>Componentes ocultos</p>
+    }
+
   </>
 }
 
